cfdfd01a59843de85ebd2c2fd24f49473599325c
jess2 <jess2.jsy@gmail.com>
2019-11-19 22:13:18 +0900

    add solution

--- leetcode/116-populating-next-right-pointers-in-each-node-jessie.js
+++ leetcode/116-populating-next-right-pointers-in-each-node-jessie.js
@@ -1,16 +1,3 @@
-/**
- * // Definition for a Node.
- * function Node(val,left,right,next) {
- *    this.val = val;
- *    this.left = left;
- *    this.right = right;
- *    this.next = next;
- * };
- */
-/**
- * @param {Node} root
- * @return {Node}
- */
 var connect = function(root) {
     if (root === null){
         return;
